38f486b7fec3980e12f20f3f91650f1c2608df5d
Dave Laptop <davejprince_@hotmail.com>
2015-11-11 11:06:01 +0100

    Ticket #6 is geimplementeerd. De stijl is erop aangepast. De CSS is opgeschoond.

--- Lisa.Excelsis.WebClient/wwwroot/assessment/start.js
+++ Lisa.Excelsis.WebClient/wwwroot/assessment/start.js
@@ -36,4 +36,34 @@ export class Start{
             this.newTime = this.assessment.assessed.time;
         });
     }   
+    getAssessment(id){
+        this.http.get("assessments/"+id).then(response => {
+            this.assessment = response.content; 
+            this.name = this.assessment.student.name;
+            this.number = this.assessment.student.number;
+            this.newDate = this.assessment.assessed.date;
+            this.newTime = this.assessment.assessed.time;
+
+            for(var i = 0; i < this.assessment.observations.length; i++){                
+                this.assessment.observations[i].result = "notRated";
+            }
+        });
+    }  
+
+    criteriumAnswerButton(id, name){
+        for(var i = 0; i < this.assessment.observations.length; i++){ 
+            if(this.assessment.observations[i].id == id){
+                if((this.assessment.observations[i].result == "done" && name == "done") || (this.assessment.observations[i].result == "notDone" && name == "notDone")){
+                    this.assessment.observations[i].result = "notRated";
+                }
+                else if((this.assessment.observations[i].result == "done" && name != "done") || (this.assessment.observations[i].result == "notRated" && name != "done")){
+                    this.assessment.observations[i].result = "notDone";
+                }
+                else if((this.assessment.observations[i].result == "notDone" && name != "notDone") || (this.assessment.observations[i].result == "notRated" && name != "notDone")){
+                    this.assessment.observations[i].result = "done";
+                }
+
+            }
+        }
+    }
 }
