83c91e6081d14dfc1da37ed1a8264a35573a2700
Dave Laptop <davejprince_@hotmail.com>
2015-11-12 11:28:21 +0100

    In de database een extra kolom toegevoegd met een uitleg over de vraag. Een knop voor uitleg over de vraag toegevoegd. jQuery geimplementeerd

--- Lisa.Excelsis.Data/Models/Criterium.cs
+++ Lisa.Excelsis.Data/Models/Criterium.cs
@@ -8,6 +8,7 @@ namespace Lisa.Excelsis.Data
         public int Id { get; set; }
         public int Order { get; set; }
         public string Description { get; set; }
+        public string Explanation { get; set; }
         public string value { get; set; }
         public int ExamId { get; set; }
     }
